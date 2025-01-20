3a09e6a36fdf0147a16cd7c49df73f99ee29d211
AdlaSurya501 <159685837+AdlaSurya501@users.noreply.github.com>
2025-01-20 21:44:14 +0530

    Create Week2Q10.js

---
+++ Week2Q10.js
@@ -0,0 +1,5 @@
+// How can you swap two variables using array destructuring? Write an example that swaps 
+// the values of two variables, a and b, without using a temporary variable.
+let a=10,b=20;
+[a,b]=[b,a];
+console.log(a,b);
